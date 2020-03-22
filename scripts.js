8f331283809d4ce83c0b9e7e736d45b195358f91
dapaua <paumartinm@gmail.com>
2020-03-22 13:42:19 +0100

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -1,4 +1,4 @@
-const cards = document.querySelectorAll('.memory-card');
+const cards = document.querySelectorAll('.flipcard');
 const activitatText = document.querySelectorAll('#activitat-text')[0];
 
 let lockBoard = false;
@@ -9,14 +9,23 @@ let card2=0;
 let card3=0;
 let numimages=6;
 let missatgeInicial="Prem les cartes per descobrir l'activitat."
+let soundEnabled=1;
 
 function flipCard() {
   if (lockBoard) return;
   
-
+  if (this.classList.contains('flip'))
+    {
+        //Already clicked
+        return;
+    }
   this.classList.add('flip');
-  clicksound.start();
 
+  if (soundEnabled==1)
+    {
+        new Audio("img/page-flip-01a.mp3").play()
+    }
+    
    cardsFlipped++;
   checkForFlipped();
 }
@@ -33,7 +42,23 @@ if (cardsFlipped==3)
 
 }
 
+function toggleSound()
+{
+    soundEnabled=1-soundEnabled;
+    soundIcon=document.getElementById("so-button")
 
+    if (soundEnabled==1)
+        {
+            soundIcon.classList.remove("fa-volume-mute");
+            soundIcon.classList.add("fa-volume-up");
+        }
+        else
+        {
+            soundIcon.classList.add("fa-volume-mute");
+            soundIcon.classList.remove("fa-volume-up");
+        }
+
+}
 
 function resetBoard() {
   [hasFlippedCard, lockBoard] = [false, false];
@@ -53,26 +78,14 @@ function resetBoard() {
   card2=Math.floor(Math.random() * (numimages-1))+1;
   card3=Math.floor(Math.random() * (numimages-1))+1;
   
-  var c1 = document.getElementById("c1");
-  for (var i = 0; i < c1.childNodes.length; i++) {
-      if (c1.childNodes[i].className == "front-face") {
-             myimg=c1.childNodes[i];
-             myimg.src="img/c1/"+card1+".svg";  }
-      }
+  var c1 = document.getElementById("front1");
+  c1.src="img/c1/"+card1+".png";  
+ 
+  var c2 = document.getElementById("front2");
+  c2.src="img/c2/"+card2+".png";  
 
-  var c2 = document.getElementById("c2");
-  for (var i = 0; i < c2.childNodes.length; i++) {
-      if (c2.childNodes[i].className == "front-face") {
-             myimg=c2.childNodes[i];
-             myimg.src="img/c2/"+card2+".svg";  }
-      }
-    
-      var c3 = document.getElementById("c3");
-      for (var i = 0; i < c3.childNodes.length; i++) {
-          if (c3.childNodes[i].className == "front-face") {
-                 myimg=c3.childNodes[i];
-                 myimg.src="img/c3/"+card3+".svg";  }
-          }
+  var c2 = document.getElementById("front3");
+  c2.src="img/c2/"+card2+".png";  
 
         },500);
 
@@ -81,45 +94,9 @@ function resetBoard() {
 
 resetBoard();
 document.getElementById("reinici").addEventListener('click', resetBoard);
+document.getElementById("btn-sound").addEventListener('click', toggleSound);
 cards.forEach(card => card.addEventListener('click', flipCard));
-let clicksound = new Sound("img/page-flip-01a.mp3", 100, true);
+
 
 //Randomize cards images.
 
-
-// source: https://stackoverflow.com/a/11331200/4298200
-function Sound(source, volume, loop)
-{
-    this.source = source;
-    this.volume = volume;
-    this.loop = loop;
-    var son;
-    this.son = son;
-    this.finish = false;
-    this.stop = function()
-    {
-        document.body.removeChild(this.son);
-    }
-    this.start = function()
-    {
-        if (this.finish) return false;
-        this.son = document.createElement("embed");
-        this.son.setAttribute("src", this.source);
-        this.son.setAttribute("hidden", "true");
-        this.son.setAttribute("volume", this.volume);
-        this.son.setAttribute("autostart", "true");
-        this.son.setAttribute("loop", this.loop);
-        document.body.appendChild(this.son);
-    }
-    this.remove = function()
-    {
-        document.body.removeChild(this.son);
-        this.finish = true;
-    }
-    this.init = function(volume, loop)
-    {
-        this.finish = false;
-        this.volume = volume;
-        this.loop = loop;
-    }
-}
